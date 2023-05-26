dc9d1a36d51d6e54523639d4c326f49ea429422b
TimAbuev <tim-r10@yandex.ru>
2023-05-26 23:24:10 +0300

    feat: add 2 user's controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -7,4 +7,26 @@ function createTestUser(req, res, next) {
     .catch((err) => next(err));
 }
 
-module.exports = { createTestUser };
+function getUserInfo(req, res, next) {
+  return User.find({})
+    .then((user) => res.status(200).send(user))
+    .catch((err) => next(err));
+}
+
+function updateUser(updateData) {
+  return (req, res, next) => {
+    User.findByIdAndUpdate(
+      '6470bafccb15655b6a8a4727',
+      updateData(req),
+      { new: true, runValidators: true },
+    )
+      .then(() => {
+        res.status(200).send(req.body);
+      })
+      .catch((err) => next(err));
+  };
+}
+
+const updateProfile = updateUser((req) => ({ name: req.body.name, email: req.body.email }));
+
+module.exports = { createTestUser, getUserInfo, updateProfile };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { createTestUser } = require('../controllers/usersControllers'); // важно соблюдать порядок импортируемых объектов
+const { createTestUser, getUserInfo, updateProfile } = require('../controllers/usersControllers'); // важно соблюдать порядок импортируемых объектов
 
 router.post('/me', createTestUser);
+router.get('/me', getUserInfo);
+router.patch('/me', updateProfile);
 
 module.exports = router;
