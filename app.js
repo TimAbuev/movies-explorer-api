d4e996fdf9a2ad5857ad4a036c9e90d466a91e7c
TimAbuev <tim-r10@yandex.ru>
2023-07-06 09:38:26 +0300

    fix: app.js CORS

--- app.js
+++ app.js
@@ -16,10 +16,9 @@ const app = express();
 
 // app.use(allowCORS);
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
 }));
 
 app.use(express.json());
